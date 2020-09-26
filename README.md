9f6f38f432e3c74b381447c3bdd7efe6e8c2c707
Gaurav <shahgauravraj@gmail.com>
2020-09-26 13:57:05 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Current Weather
+
+![alt text]()
+
+Current Weather is a modern web application which give current weather status of different locations based on the name of city provided. It displays current temperature of any city in °C along with actual temperature being felt, humidity and the speed of wind in meter/second formats.
+
+## About
+This is a simple project built using HTML, CSS and JavaScript. For realtime weather information, I have made use of OpenWeatherMap API which is free to use but limited to 60 calls/minute for an unpaid plan.
+The following items are displayed on screen:
+- [x] Current temperature of the city
+- [x] Maximum and Minimum temperature
+- [X] Feels like temperature
+- [X] Humidity
+- [X] Wind speed
+
+### Project Status
+The app is ready to be shipped.
+
+### Getting Started
+
+A few useful resources for this project:
+
+- [OpenWeatherMap API](https://openweathermap.org/)
+- [API Documentation](https://openweathermap.org/current)
---
+++ main.js
@@ -0,0 +1,60 @@
+const api = {
+  key: "7de0d921408986d71962bcb7868b7332",
+  base: "http://api.openweathermap.org/data/2.5/"
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+  if (evt.keyCode == 13) {
+    getResults(searchbox.value);
+    document.activeElement.blur();
+  }
+}
+
+function getResults(query) {
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+      return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults(weather) {
+  let city = document.querySelector('.location .city');
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  let date = document.querySelector('.location .date');
+  date.innerText = dateBuilder(now);
+
+  let temp = document.querySelector('.current .temp');
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+
+  let weather_el = document.querySelector('.current .weather');
+  weather_el.innerText = weather.weather[0].main;
+
+  let hilow = document.querySelector('.hi-low');
+  hilow.innerText = `${Math.round(weather.main.temp_max)}°c / ${Math.round(weather.main.temp_min)}°c`;
+
+  let feelslike = document.querySelector('.current .feels-like');
+  feelslike.innerText = `Feels like: ${Math.round(weather.main.feels_like)}°c`;
+
+  let humidity = document.querySelector('.current .humidity');
+  humidity.innerText = `Humidity: ${weather.main.humidity}%`;
+
+  let windSpeed = document.querySelector('.current .wind');
+  windSpeed.innerText = `Wind speed: ${weather.wind.speed} m/s`;
+}
+
+function dateBuilder(d) {
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+}
